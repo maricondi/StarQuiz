d02d45804ea7237ef91edcec1de19aefc69db6b2
Maricondi <maricondiwielki@gmail.com>
2019-05-06 18:34:53 -0300

    first commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class App extends Component {
+  render() {
+    return (
+      <div>
+          first commit
+      </div>
+    );
+  }
+}
+
+export default App;
